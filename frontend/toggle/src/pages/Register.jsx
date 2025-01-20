bd4cc5afdb5f8b793dc6cb6c434fd4be4edf5599
Lavish098 <75539589+Lavish098@users.noreply.github.com>
2025-01-20 10:38:30 +0100

    Update Register.jsx

--- frontend/toggle/src/pages/Register.jsx
+++ frontend/toggle/src/pages/Register.jsx
@@ -22,7 +22,7 @@ function Register() {
       <div className="w-full bg-white rounded-lg">
         <div className="p-6 space-y-4 md:space-y-6 sm:p-8">
           <h1 className="text-xl font-bold leading-tight">
-            Sign up fo an account
+            Sign up for an account
           </h1>
           <form
             onSubmit={handleSignUpSubmit}
