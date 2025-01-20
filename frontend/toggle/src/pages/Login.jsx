1e3c82ff20a12421830840355fb6733872204e7d
Lavish098 <75539589+Lavish098@users.noreply.github.com>
2025-01-20 10:37:56 +0100

    Update Login.jsx

--- frontend/toggle/src/pages/Login.jsx
+++ frontend/toggle/src/pages/Login.jsx
@@ -16,7 +16,7 @@ const Login = () => {
       <div className="w-full bg-white rounded-lg">
         <div className="p-6 space-y-4 md:space-y-6 sm:p-8">
           <h1 className="text-xl font-bold leading-tight">
-            Sign up fo an account
+            Sign in to your account
           </h1>
           <form
             onSubmit={handleLoginSubmit}
